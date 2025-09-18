4f7c5d88291649eb3da980ca14cb3b13d9545d64
24DP1AVasi <a240731av@rvt.lv>
2025-09-18 12:58:33 +0300

    Addition formula

---
+++ src/main/java/rvt/Addition.java
@@ -0,0 +1,19 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int num1 = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int num2 = Integer.valueOf(scanner.nextLine());
+
+        scanner.close();
+
+        System.out.println(num1+" + "+num2+" = "+ (num1 + num2));
+    }
+}
