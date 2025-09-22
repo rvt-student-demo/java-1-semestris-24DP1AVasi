d6e0666231c0cb3b7b39295b83a57f7181b33441
24DP1AVasi <a240731av@rvt.lv>
2025-09-22 12:30:17 +0300

    Stundas darbs

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,33 @@
 package rvt;
+import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadi num:");
+
+        int num = Integer.valueOf(scanner.nextLine());
+
+        scanner.close();
+
+        if (num % 3 == 0 && num % 5 != 0) {
+            System.out.println("Fizz");
+        } 
+        else if (num % 5 == 0 && num % 3 != 0) {
+            System.out.println("Buzz");
+        }
+        else if (num % 3 == 0 && num % 5 == 0) {
+            System.out.println("FizzBuzz");
+        }
+        // if (age > 18) {
+        //     System.out.println("Tu esi pilngadīgs");
+        // }
+
+        // else {
+        //     System.out.println("Tu nevari lietot programmu");
+        // }
+        
     }
 }
