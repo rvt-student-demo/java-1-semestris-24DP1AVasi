ec6d520ca2b02be9c2ef81f05a1d64a7ba25b88e
24DP1AVasi <a240731av@rvt.lv>
2025-09-24 09:28:07 +0300

    Delikatese

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -4,23 +4,40 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println("Ievadi num:");
+        System.out.print("Ievadi preci: ");
+        String prece = String.valueOf(scan.nextLine());
+        System.out.print("Ievadi cenu: ");
+        double cena = Double.valueOf(scan.nextLine());
+        System.out.print("Ekspress piegāse (0=nē, 1=jā): ");
+        int pieg = Integer.valueOf(scan.nextLine());
 
-        int num = Integer.valueOf(scanner.nextLine());
-
-        scanner.close();
-
-        if (num % 3 == 0 && num % 5 != 0) {
-            System.out.println("Fizz");
-        } 
-        else if (num % 5 == 0 && num % 3 != 0) {
-            System.out.println("Buzz");
+        if (pieg == 1) {
+            pieg = 3;
         }
-        else if (num % 3 == 0 && num % 5 == 0) {
-            System.out.println("FizzBuzz");
+        else {
+            pieg = 0;
         }
+        if (cena < 10) {
+            pieg += 2;
+        }
+        else {
+            pieg = 0;
+        }
+        System.out.println("Rēķins:\n\t"+prece+"\t"+cena+"\n\tpiegāde\t"+pieg+"\n\tkopā\t"+(cena + pieg));
+        
+        scan.close();
+
+        // if (num % 3 == 0 && num % 5 != 0) {
+        //     System.out.println("Fizz");
+        // } 
+        // else if (num % 5 == 0 && num % 3 != 0) {
+        //     System.out.println("Buzz");
+        // }
+        // else if (num % 3 == 0 && num % 5 == 0) {
+        //     System.out.println("FizzBuzz");
+        // }
         // if (age > 18) {
         //     System.out.println("Tu esi pilngadīgs");
         // }
