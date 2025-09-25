fa58e0a1d8ff0ec3f84afdac47a9de424217ff52
24DP1AVasi <a240731av@rvt.lv>
2025-09-25 13:31:19 +0300

    NumberadnSum

---
+++ src/main/java/rvt/NumberAndSumOfNumbers.java
@@ -0,0 +1,17 @@
+package rvt;
+import java.util.Scanner;
+
+
+public class NumberAndSumOfNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Ievadi :");int num = Integer.valueOf(scan.nextLine());int sum = num;int sk = 0;
+        while (num != 0) {
+            System.out.println("Ievadi skaitli:");num = Integer.valueOf(scan.nextLine());sum += num;sk += 1;
+        }
+        System.out.println("Skaitlu daudzums " + sk);
+        System.out.println("Tava summa ir " + sum);
+
+    }
+}
