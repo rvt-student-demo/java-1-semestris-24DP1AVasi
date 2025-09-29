9ee94605076b824a1ed79da418e4077df8449ffa
24DP1AVasi <a240731av@rvt.lv>
2025-09-29 11:08:16 +0300

    Spēle

---
+++ src/main/java/rvt/Spele.java
@@ -0,0 +1,23 @@
+package rvt;
+
+
+import java.util.Random;
+import java.util.Scanner;
+public class Spele {
+    public static void main(String[] args) {
+    Random ran = new Random();
+    Scanner sca = new Scanner(System.in);
+    int res = 0;
+    System.out.println("Es domāju par skaitli no 1 līdz 10.\nTev tas jauzmin trīs mēģinājumos.\nIevadi minējumu:");
+    for (int i = 0; i == 3; i++)
+        res = ran.nextInt(1,11);
+        int inp = Integer.valueOf(sca.nextLine());
+        if (inp != res) {
+            System.out.println("Nepareizi");
+        }
+        else {
+            System.out.println("Pareizi\nTu uzvarēji")
+            break;
+        }
+    }
+}
