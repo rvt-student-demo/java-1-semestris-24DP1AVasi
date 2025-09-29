b6950beac34537522350ebb3262dbb4b9e13c3bf
24DP1AVasi <a240731av@rvt.lv>
2025-09-29 11:13:39 +0300

    Spēle

--- src/main/java/rvt/Spele.java
+++ src/main/java/rvt/Spele.java
@@ -9,7 +9,7 @@ public class Spele {
     Scanner sca = new Scanner(System.in);
     int res = 0;
     System.out.println("Es domāju par skaitli no 1 līdz 10.\nTev tas jauzmin trīs mēģinājumos.\nIevadi minējumu:");
-    for (int i = 0; i == 3; i++)
+    for (int i = 0; i == 3; i++) {
         res = ran.nextInt(1,11);
         int inp = Integer.valueOf(sca.nextLine());
         if (inp != res) {
@@ -17,6 +17,8 @@ public class Spele {
         }
         else {
             System.out.println("Pareizi\nTu uzvarēji");
+            break;
+        }
         }
     }
 }
