c7130ac78ef609054ce642b9ed2d3165b248e34c
Armands Andrejs Vasiļjevs <vasiljevs.armands@gmail.com>
2025-10-03 11:58:04 +0300

    Py_input

--- src/main/java/rvt/PYinput.java
+++ src/main/java/rvt/PYinput.java
@@ -3,7 +3,8 @@ package rvt;
 
 import java.util.Scanner;
 public class PYinput {
-
+    
+    // String
     public static String InStr(String text) {
          Scanner scan = new Scanner(System.in);
         System.out.print(text);
@@ -11,6 +12,8 @@ public class PYinput {
         scan.close();
         return str;
     }
+
+    // Integer
     public static int InInt(String text) {
          Scanner scan = new Scanner(System.in);
         System.out.print(text);
@@ -18,6 +21,8 @@ public class PYinput {
         scan.close();
         return Int;
     }
+
+    // Double
     public static Double InDou(String text) {
          Scanner scan = new Scanner(System.in);
         System.out.print(text);
